d5c6bb5f72dc1a68e9fff7c6bf12622c734578bd
Felipe Valencia <felipe.valencia.2003@gmail.com>
2023-07-24 20:01:12 -0500

    0155 Min Stack in Rust

--- rust/0155-min-stack.rs
+++ rust/0155-min-stack.rs
@@ -1,37 +1,51 @@
+/*
+ * @lc app=leetcode id=155 lang=rust
+ *
+ * [155] Min Stack
+ */
+
+
+// @lc code=start
+use std::cmp::min;
+#[derive(Debug)]
 struct MinStack {
-    stack: Vec<i32>,
-    min_stack: Vec<i32>,
+    stack: Vec<(i32, i32)>,
 }
 
+/**
+ * `&self` means the method takes an immutable reference.
+ * If you need a mutable reference, change it to `&mut self` instead.
+ */
 impl MinStack {
     fn new() -> Self {
         Self {
-            stack: vec![],
-            min_stack: vec![],
+            stack: Vec::new(),
         }
     }
 
     fn push(&mut self, val: i32) {
-        self.stack.push(val);
-        if self.min_stack.is_empty() || Some(&val) <= self.min_stack.last() {
-            self.min_stack.push(val);
+        if self.stack.is_empty() {
+            self.stack.push((val, val));
+        } else {
+            self.stack.push((val, min(self.stack.last().unwrap().1, val)))
         }
     }
 
     fn pop(&mut self) {
-        let val = self.stack.pop().unwrap();
-        if Some(&val) == self.min_stack.last() {
-            self.min_stack.pop();
-        }
+        self.stack.pop();
     }
 
     fn top(&self) -> i32 {
-        self.stack.last().cloned().unwrap()
+        self.stack.last().unwrap().0
     }
 
     fn get_min(&self) -> i32 {
-        self.min_stack.last().cloned().unwrap()
+        self.stack.last().unwrap().1
     }
 }
-
-
+// @lc code=end
+fn main() {
+    let mut stack = MinStack::new();
+    stack.push(1);
+    stack.pop();
+}
